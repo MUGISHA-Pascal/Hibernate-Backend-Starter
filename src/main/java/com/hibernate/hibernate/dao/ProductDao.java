21300e9d394b10f4098fd0f738631c38a424d4b3
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-06-12 13:14:34 +0200

    refactor : dao methods

--- src/main/java/com/hibernate/hibernate/dao/ProductDao.java
+++ src/main/java/com/hibernate/hibernate/dao/ProductDao.java
@@ -5,6 +5,8 @@ import com.hibernate.hibernate.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class ProductDao {
     public void saveProduct(Product product){
         Transaction transaction = null;
@@ -22,5 +24,34 @@ public class ProductDao {
             return session.get(Product.class,id);
         }
     }
-
+    public List<Product> getAllProducts(){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            return session.createQuery("from Product",Product.class).list();
+        }
+    }
+    public void updateProduct(Product product){
+        Transaction transaction = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            session.update(product);
+            transaction.commit();
+        }catch (Exception e){
+            if(transaction != null){transaction.rollback();}
+            e.printStackTrace();
+        }
+    }
+    public void deleteProduct(Long id){
+        Transaction transaction = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            Product product = session.get(Product.class, id);
+            if (product != null) {
+                session.delete(product);
+                transaction.commit();
+            }
+        }catch (Exception e){
+            if(transaction != null){transaction.rollback();}
+            e.printStackTrace();
+        }
+    }
 }
